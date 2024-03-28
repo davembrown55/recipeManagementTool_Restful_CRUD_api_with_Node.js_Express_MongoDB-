3c4bf0f3b3073e56ffd90de2a7557888b19e1cfb
Dave Brown <davembrown55@googlemail.com>
2024-03-28 17:41:34 +0000

    add navbar to app.js/ installed axios / create recipeService.js / create http-common.js

--- Frontend/recipe-management-react-crud/src/App.js
+++ Frontend/recipe-management-react-crud/src/App.js
@@ -3,7 +3,36 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
-    // ...
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/recipes" className="navbar-brand">     
+          Recipe Management Tool       
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/recipes"} className="nav-link">
+                Recipes
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Routes>
+            <Route path="/" element={<RecipesList/>} />
+            <Route path="/recipes" element={<RecipesList/>} />
+            <Route path="/add" element={<AddRecipe/>} />
+            <Route path="/recipes/:id" element={<Recipe/>} />
+          </Routes>
+        </div>
+      </div>
+    );
   }
 }
 
