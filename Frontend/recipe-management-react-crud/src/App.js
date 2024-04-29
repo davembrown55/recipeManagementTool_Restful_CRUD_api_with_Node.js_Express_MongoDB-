b3e84f62a90f7b7c246ec5bc81b39188e61af8e0
Dave Brown <davembrown55@googlemail.com>
2024-04-29 17:09:34 +0100

    Updates to App.js: added react bootstrap containers instead of divs. Added highlightRecipesNavEmptyPath function to highlight Recipes nav item when path is: '/'. Updates to Nav.Link items to as={Link}. This has stopped the page refreshing when a new component has been loaded from user.

--- Frontend/recipe-management-react-crud/src/App.js
+++ Frontend/recipe-management-react-crud/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route, Link, useLocation } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
 import Container from 'react-bootstrap/Container';
@@ -20,35 +20,44 @@ class App extends Component {
   render() {
 
     const { location } = this.props.router;
+    // console.log(location.pathname);
+
+    function highlightRecipesNavEmptyPath() {
+      if (location.pathname == '/') {
+        return "/recipes";        
+      } else {
+        return location.pathname;
+      }
+    }
 
     return (
-      <div>
+      
+      <Container fluid className = "p-0">   
 
-          <Navbar expand="lg" bg="dark" data-bs-theme="dark"> 
+        <Navbar expand="lg" bg="dark" data-bs-theme="dark"> 
           <Container fluid>
-            <Navbar.Brand href={"/recipes"}>Recipe Management Tool</Navbar.Brand>
+            <Navbar.Brand as={Link} to={"/recipes"}>Recipe Management Tool</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto" variant="underline" activeKey={location.pathname} > 
-              <Nav.Link href={"/recipes"} >Recipes</Nav.Link>
-              <Nav.Link href={"/add"} >Add</Nav.Link>
+            <Nav className="me-auto" variant="underline" activeKey={highlightRecipesNavEmptyPath()} > 
+              <Nav.Link as={Link} to={"/recipes"} href={"/recipes"}>Recipes</Nav.Link>
+              <Nav.Link as={Link} to={"/add"} href={"/add"}>Add</Nav.Link>
             </Nav>
             </Navbar.Collapse>
 
           </Container>            
         </Navbar>          
         
-
-        <div className="container mt-3">
+        <Container fluid className = "mt-5">         
           <Routes>
             <Route path="/" element={<RecipeList/>} />
             <Route path="/recipes" element={<RecipeList/>} />
             <Route path="/add" element={<AddRecipe/>} />
             <Route path="/recipes/:id" element={<Recipe/>} />
-          </Routes>
-        </div>
-      </div>
-      
+          </Routes>        
+        </Container>      
+
+      </Container>
     );
   }
 }
